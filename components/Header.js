91781a43a7c118b25167983bb2de24eba088a730
ABY <abyener54@gmail.com>
2021-10-22 18:13:13 +0300

    Completed Header Component

--- components/Header.js
+++ components/Header.js
@@ -7,34 +7,49 @@ import {
     PaperAirplaneIcon,
     MenuIcon
 } from "@heroicons/react/outline"
+import { HomeIcon } from "@heroicons/react/solid"
 
 function Header() {
     return (
-        <div>
-            <div className="flex justify-between max-w-6xl">
+        <div className="shadow-sm border-b bg-white sticky top-0 z-50">
+            <div className="flex justify-between max-w-6xl mx-5 lg:mx-auto">
                 {/* Left */}
-                <div className="relative hidden lg:inline-grid h-24 w-24 cursor-pointer">
+                <div className="relative hidden lg:inline-grid  w-24 cursor-pointer">
                     <Image src="https://links.papareact.com/ocw"
                         layout="fill" objectFit="contain"
                     />
                 </div>
 
-                <div className="relative h-10 w-10 lg:hidden flex-shrink-0 cursor-pointer">
+                <div className="relative w-10 lg:hidden flex-shrink-0 cursor-pointer">
                     <Image src="https://links.papareact.com/jjm"
                         layout="fill" objectFit="contain" 
                     />
                 </div>
 
                 {/* Middle - Search input*/}
-
-                <div>
-                    <div>
-
+                <div className="max-w-xs">
+                    <div className="relative mt-1 p-3 rounded-md">
+                        <div className="absolute insert-y-0 pl-3 flex items-center pointer-events-none">
+                            <SearchIcon className="h-5 w-5 mt-2 text-gray-500" />
+                        </div>
+                        <input className="bg-gray-50 block w-full pl-10 sm:text-sm border-gray-300 focus:ring-black focus:border-black rounded-md" type="text" placeholder="Search" /> 
                     </div>
-                    <input type="text" placeholder="Search" /> 
                 </div>
-
                 {/* Right */}
+                <div className="flex items-center justify-end space-x-4">
+                    <HomeIcon className="navBtn" />
+                    <MenuIcon className="h-6 md:hidden cursor-pointer"
+                    />
+                    <div className="relative navBtn">
+                        <PaperAirplaneIcon className="navBtn rotate-45"/>
+                        <div className="absolute -top-1 -right-2 text-xs w-5 h-5 bg-red-500 rounded-full flex item-center justify-center animate-pulse text-white">3</div>
+                    </div>
+                    <PlusCircleIcon className="navBtn"/>
+                    <UserGroupIcon className="navBtn"/>
+                    <HeartIcon className="navBtn"/>
+                    
+                    <img src="https://i1.sndcdn.com/avatars-000191864169-xzno4r-t240x240.jpg" alt="proile pic" className="h-10 rounded-full cursor-pointer" />
+                </div>
             </div>
         </div>
     )
